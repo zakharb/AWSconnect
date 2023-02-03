b1f82cb88cde61a84cf539031c09db2294fd1ff3
ze <zakharbengart@gmail.com>
2023-02-03 06:41:44 +0400

    add workflow

--- awsclient/__main__.py
+++ awsclient/__main__.py
@@ -1,6 +1,28 @@
 """
-	AWS client to work with EC2 and S3 instances
+    AWSClient
+    Copyright (C) 2022
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Author:
+        Bengart Zakhar
+
+    Description:
+        Main function to start AWS Client
+        
 """
+
 import json
 import argparse
 from awsclient.ec2 import EC2
--- awsclient/sshclient.py
+++ awsclient/sshclient.py
@@ -1,3 +1,28 @@
+"""
+    AWSClient
+    Copyright (C) 2022
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Author:
+        Bengart Zakhar
+
+    Description:
+        Class to work with SSHClient
+        
+"""
+
 import paramiko
 from pathlib import Path
 
